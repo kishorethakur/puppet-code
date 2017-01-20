1b77810a35c5770f0c23e5290ae755c5c637c06d
Kishore Thakur <kishorethakur.rajput@gmail.com>
2017-01-20 16:23:33 +0530

    modified site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,5 @@
 # site.pp file for production env
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # example:
-  #   class { 'my_class': }
-#  include apache
+  include root
 }
