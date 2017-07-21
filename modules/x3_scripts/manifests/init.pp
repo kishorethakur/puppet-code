625cfd5024b7e36cefeb2914d168d76ee983a73c
Kishore Thakur <kishorethakur.rajput@gmail.com>
2017-07-22 00:50:24 +0530

    new modules x3_scripts

---
+++ modules/x3_scripts/manifests/init.pp
@@ -0,0 +1,11 @@
+class x3_scripts {
+     file { '/scripts':
+          ensure  => directory,
+          mode    => '0744',
+          owner   => 'root',
+          group   => 'root',
+          source  => 'puppet:///scripts',
+          recurse => 'remote',
+          path    => '/scripts',
+     }
+
