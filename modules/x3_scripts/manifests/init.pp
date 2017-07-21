30a2f7d12804c582a21905ad0c827902a2046dac
Kishore Thakur <kishorethakur.rajput@gmail.com>
2017-07-22 01:15:29 +0530

    fixed syntax issues

--- modules/x3_scripts/manifests/init.pp
+++ modules/x3_scripts/manifests/init.pp
@@ -4,8 +4,7 @@ class x3_scripts {
           mode    => '0744',
           owner   => 'root',
           group   => 'root',
-          source  => 'puppet:///.../scripts',
-          recurse => 'remote',
-          path    => '/scripts',
+          recurse =>  'remote',
+          source  => 'puppet:///modules/x3_scripts/files/scripts',
      }
 }
